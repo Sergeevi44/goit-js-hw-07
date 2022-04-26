6d5b8153361f9c82bf4d8015e13c90a5656793c0
Oleksii <sergeevi44@gmail.com>
2022-04-26 14:08:03 +0300

    without close by Esc

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const refs = {
+	galleryList: document.querySelector('.gallery'),
+}
 
-console.log(galleryItems);
+refs.galleryList.insertAdjacentHTML('beforeend', renderMarkup(galleryItems))
+refs.galleryList.addEventListener('click', onImgClick)
+
+function renderMarkup(itemsArray) {
+    return itemsArray.map(({original, preview, description})=>
+        `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`
+    ).join('')
+}
+function onImgClick(e) {
+	e.preventDefault()
+	if (e.target.tagName!='IMG') {
+		return
+	}
+	const original = `<img src="${e.target.dataset.source}"/>`;
+	addEventListener('keydown', onEscPress)
+	return basicLightbox.create(original).show()
+}
+function onEscPress(e) {
+	if (e.key != 'Escape') {
+		return
+	}
+	removeEventListener('keydown', onEscPress);
+}
